1a7c68a759b34687e274890d76e2ee27d2c12052
Eliseo Arica <eliseo989665091@gmail.com>
2022-08-02 16:00:06 -0500

    Separado el componente LaunchItem y diseño mejorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import * as API from "./services/launches.js";
-import { HiCalendar } from "react-icons/hi";
-import { Heading, Box, Image, Flex, Text, Spacer, Tag } from '@chakra-ui/react';
-import dayjs from "dayjs";
-import "dayjs/locale/es"
+import { Heading, Image } from '@chakra-ui/react';
 import { useState, useEffect } from "react";
 import logo from "./assets/spacex-vector-logo.png";
+import { LaunchItem } from "./components/LaunchItem";
+
+
 // Exportamos de manera nombrada
 export function App() {
   const [launches, setLaunches] = useState([]);
@@ -13,7 +13,6 @@ export function App() {
     // La línea X es la forma abreviada de:
     // API.getAllLaunches().then( datos => setLaunches(datos));
     API.getAllLaunches().then(setLaunches);
-    console.log("rendered");
   }, []);
 
   return (
@@ -22,26 +21,7 @@ export function App() {
       <Heading as="h1" size="lg" m={4}>SpaceX Launches</Heading>
         <section>
           {launches.map( (launch) => ( // <- Nótese el paréntesis "(" en vez de "{"
-            <Box key={launch.flight_number} bg="gray.100" p={4} m={4} borderRadius="lg">
-              <Flex display="flex">
-                <Text fontSize="xl">
-                  Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
-                </Text>
-                <Spacer />
-                <Tag bg={launch.launch_success ? "green.400" : "red.400"} color="white">
-                  {launch.launch_success ? "SUCCESS" : "FAILURE"}
-                </Tag>
-              </Flex>
-
-              <Flex align="center">
-                <HiCalendar />
-                <Text fontSize="sm" ml={1}>
-                  {dayjs(launch.launch_date_local.split("T")[0])
-                  .locale("es")
-                  .format('D [de] MMMM [de] YYYY')}
-                </Text>
-              </Flex>
-            </Box>
+            <LaunchItem key={launch.flight_number} {...launch} />
           ))}
         </section>
     </>  
